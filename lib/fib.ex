c72f1125c84081b93b5e4d9218e5921f5f8306c3
Jerry Hilts <gwhilts@gmail.com>
2016-06-03 01:26:52 -0700

    Re-implements Fib module
    Didn't like the first stab I took at the
    "Even Fibonacci Numbers" kata. Think this is
    a much better implementation, though there are
    probably still more elegant solutions.

--- lib/fib.ex
+++ lib/fib.ex
@@ -1,18 +1,21 @@
 defmodule Fib do
 
-  def fib(0), do: 1
-  def fib(1), do: 1
+  def fib(n) when n < 2, do: 0
+  def fib(n) when n < 4, do: 1
   def fib(n), do: fib(n-1) + fib(n-2)
 
   def sum_of_evens_to(limit) do
-    Enum.reduce_while(1..1_000_000, 0, fn n, sum ->        
-     f = fib(n)
-     new_sum = 
-       case rem(f, 2) do
-         0 -> sum + f
-         1 -> sum
-       end
-     if f <= limit, do: {:cont, new_sum}, else: {:halt, new_sum}
-    end) 
+    calc_sum(1, 0, 0, limit)
+  end
+
+  defp calc_sum(_, fib_val, sum, limit) when fib_val > limit do
+    sum
+  end
+
+  defp calc_sum(n, fib_val, sum, limit) do
+    case rem fib_val, 2 do
+      0 -> calc_sum(n+1, fib(n), sum + fib_val, limit)
+      1 -> calc_sum(n+1, fib(n), sum, limit)
+    end
   end
 end
--- test/fib_test.exs
+++ test/fib_test.exs
@@ -3,12 +3,16 @@ defmodule FibTest do
   doctest Fib
 
   test "it calculates the n-th number in the fibonacci sequence" do
-    assert Fib.fib(3) == 3
-    assert Fib.fib(8) == 34
+    # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
+    assert Fib.fib(4)  == 2
+    assert Fib.fib(10) == 34
   end
 
   test "it calculates the sum of the even fibonacci numbers" do
-    assert Fib.sum_of_evens_to(34) == 44
-    assert Fib.sum_of_evens_to(90) == 188
+    # _, _, _, 2, _, _, 8, _, _, 34, _, _
+    # 2 + 8 + 34
+    # 44
+    assert Fib.sum_of_evens_to(10) == 10
+    assert Fib.sum_of_evens_to(90) == 44
   end
 end
