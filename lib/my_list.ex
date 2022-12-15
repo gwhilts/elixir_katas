4a927518a8b876e1ee0bba75d5a0be5a5bc0c6df
Gerald Hilts <gwhilts@booknowl.com>
2022-12-15 12:22:16 -0800

    WIP: 'Enum Functions' exercise - each/2
    Implements each/2 function.
    
    Not sure how to test this because the return is always :ok.
    Code in my_list_test doen't work. Tried using an IO.puts/1 w/
    ExUnit.CaptureIO, but it isn't working because it feeds the
    :ok result to capture_io/1 along with the stdout capture. There's
    probably some simple workaround for this, but I can't think of it
    at the moment.
    
    Doctest works and is passing (though the screen output is annoying).

--- lib/my_list.ex
+++ lib/my_list.ex
@@ -14,6 +14,24 @@ defmodule MyList do
   def any?([], _), do: false
   def any?([h | tail], f), do: f.(h) or any?(tail, f)
 
+  @spec each([any], fun) :: :ok
+  @doc """
+    invokes the given fun for each element in a list.
+
+    ## Examples
+
+    iex> MyList.each(["a", "b", "c"], & IO.puts(String.upcase(&1)))
+    A
+    B
+    C
+    :ok
+  """
+  def each([], _), do: :ok
+  def each([h | tail], f) do
+    f.(h)
+    each(tail, f)
+  end
+
   @spec flatten([any]) :: [any]
   @doc """
     returns a flat list containing all the elements of a given list containing any number of nested sublists.
