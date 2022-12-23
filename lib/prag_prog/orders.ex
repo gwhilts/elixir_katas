4bbfb68ed7ff6017e1e797ed795195977d7cfa88
Gerald Hilts <gwhilts@booknowl.com>
2022-12-23 13:54:23 -0800

    Add 'StringsAndBinaries-6: Capitalize Sentences' exercise to README

--- lib/prag_prog/orders.ex
+++ lib/prag_prog/orders.ex
@@ -4,7 +4,7 @@ defmodule PragProg.Orders do
     Enum.map(orders, fn order -> add_tax(order, tax_rates) end)
   end
 
-  defp add_tax(order, tax_rates) do
+  defp add_tax(order, tax_rates) when is_list(order) do
     sales_tax = (tax_rates[order[:ship_to]] || 0) * order[:net_amount]
     order ++ [total_amount: Float.round(order[:net_amount] + sales_tax, 2)]
   end
